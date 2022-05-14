e01b86bc958e0345658e91d65a35ab5687a122e4
caseysands <casey.sands@kellogg.northwestern.edu>
2022-05-14 22:50:05 +0000

    posts on place page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,10 +5,37 @@ class PlacesController < ApplicationController
     end
 
     def show
-        @place = Place.find_by({"id" => params{"id"}})
+        @place = Place.find_by({"id" => params["id"]})
+        @posts = Post.where({ "place_id" => @place["id"]})
     end
 
     def new
         @place = Place.new
     end
+
+    def create
+        @place = Place.new
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+    end
+
+    def edit
+        @place = Place.find_by({"id" => params["id"]})
+    end
+
+    def update
+        @place = Place.find_by({"id" => params["id"]})
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+    end
+
+    def destroy
+        @place = Place.find_by({"id" => params["id"]})
+        @place.destroy
+        redirect_to "/places"
+    end
+
+
 end
